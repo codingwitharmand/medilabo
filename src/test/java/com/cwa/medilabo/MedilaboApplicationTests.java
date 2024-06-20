6b53add146f29850971156a407974d950509d317
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2024-06-20 20:58:03 +0200

    Init repo

---
+++ src/test/java/com/cwa/medilabo/MedilaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwa.medilabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
